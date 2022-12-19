aec3a6f88753cda50516d126235ed48200b96402
MORSALIN05 <mdmsarker04@gmail.com>
2022-12-19 16:28:28 +0600

    all commit by me practice2

---
+++ pr2.js
@@ -0,0 +1,6 @@
+const p = [1, 2, 3]; const q = p.map(n => Math.pow(n, 3));
+const f = [1, 2, 3]; const r = f.filter(n => Math.pow(n, 3));
+// console.log(p);
+// console.log(r);
+const array = { hobbies: ["dancing", "singing", "acting"] };
+console.log(JSON.stringify(array));
---
+++ recap.js
@@ -0,0 +1,11 @@
+//1. default parameter
+
+//2. template string
+const newElementHTML = `
+    <div>
+        <h3>Name:</h3>
+        <p>Address:<p/>
+        <p>Numbers: ${Number}<p/>
+    </div>
+
+`
